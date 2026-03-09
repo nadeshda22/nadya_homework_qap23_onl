1cc232343c674110a2d7dd2eaa7977aa88e686ad
Nadya Yanushevskaya <nadya22.08.04@gmail.com>
2026-03-09 23:03:17 +0300

    Merge pull request #2 from nadeshda22/lesson3 изменения кода на стандарт PEP8, дополнения README и создания новой ветки
    создание новой ветки lesson3  реализация домашнего задания и заданий из лекции

--- lesson 3/1_4.py
+++ lesson 3/1_4.py
@@ -1,8 +1,7 @@
-first_people = int(input('Введите возраст первого соседа: '))
-second_people = int(input('Введите возраст второго соседа: '))
-third_people = int(input('Введите возраст третьего соседа: '))
+first_people = int(input("Введите возраст первого соседа: "))
+second_people = int(input("Введите возраст второго соседа: "))
+third_people = int(input("Введите возраст третьего соседа: "))
 
 data = (first_people, second_people, third_people)
-print(f'Сумма: {first_people + second_people + third_people}')
-print(f'Среднее арифметическое: {sum(data) / len(data)} ')
-
+print(f"Сумма: {sum(data)}")
+print(f"Среднее арифметическое: {sum(data) / len(data)} ")
